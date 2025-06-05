135dd0b751bb88878b87781ad159ba82033d2bc9
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-06-05 11:38:27 +0300

    index.js added

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2 
+console.log(multiply);
+
+
+
+let random =Math.floor(Math.random ()* 58) + 1 ;
+console.log(random);
+
+let num3 =36
+let num4 =8
+let mod=num3 % num4
+console.log(mod);
+
+let max= Math.max(10,15,3,5,19,20,8,7,3,2,1,5,8,9,0,)
+console.log(max);
+
